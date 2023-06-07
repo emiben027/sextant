18f02770d10f8b6f6564f38cbf5191f0c1d07a12
emiben027 <emmie027@gmail.com>
2023-06-07 12:35:47 +0100

    components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-        <h1>Hello Cisco !!!</h1>
-        </div>
-        <p>
-          Edited <code>src/App.js</code> saved and reloaded.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React with Cisco
-        </a>
-      </header>
+function Banner() {
+  return(
+    <div>
+      <h1>SEXTANT</h1>
     </div>
   );
 }
 
-export default App;
+function Metrics() {
+  return(
+    <div>
+      <>
+        <h4> Communication Metrics</h4>
+      </>
+        <>
+          <table>
+           <tr>
+             <th>Com Port</th>
+           </tr>
+           <tr>
+             <th>Router</th>
+           </tr>
+           <tr>
+            <th>System 1</th>
+           </tr>
+          </table>
+        </>
+    </div>
+  );
+}
+
+function Livefeed() {
+  return(
+    <div>
+      <>
+       <h4>System Connection Feed</h4>
+      </>
+      <>
+        <div class="row">
+          <div class="column">
+             <div class="card">Port 1</div>
+          </div>
+          <div>
+             <div class="card">Port 2</div>
+          </div>
+          <div>
+            <div class="card">Port 3</div>
+          </div>
+        </div>
+      </>
+    </div>
+  );
+}
+
+function Switches() {
+  return(
+    <div>
+       <>
+          <h4>System Port Switch</h4>
+       </>
+       <>
+         <h5>Port 1:</h5><button>ON/OFF</button>
+         <h5>Port 2:</h5><button>ON/OFF</button>
+         <h5>Port 3:</h5><button>ON/OFF</button>
+       </>
+    </div>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+    <Banner />
+    <h4>You're Welcome</h4>
+    <hr/>
+    <Metrics />
+    <hr/>
+    <Livefeed />
+    <hr/>
+    <Switches />
+    </div>
+  );
+}
